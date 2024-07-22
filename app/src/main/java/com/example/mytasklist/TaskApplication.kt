cc3365628b855b2903795eac544ad79f04cca690
hugonscm <hugo.nscm@outlook.com.br>
2024-07-22 11:34:13 -0300

    refatoração

--- app/src/main/java/com/example/mytasklist/TaskApplication.kt
+++ app/src/main/java/com/example/mytasklist/TaskApplication.kt
@@ -4,15 +4,14 @@ import android.app.Application
 import com.example.mytasklist.di.appModule
 import com.example.mytasklist.di.dbModule
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
 
 class TaskApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidLogger()
-            androidContext(this@TaskApplication)
+            //androidLogger(level = Level.DEBUG) //ver logs do koin
+            androidContext(this@TaskApplication) //contexto para o ThemeViewModel
             modules(appModule, dbModule)
         }
     }
