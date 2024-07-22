4be6d16a393362b7fdfc67d154a0e12f58f6659f
hugonscm <hugo.nscm@outlook.com.br>
2024-07-22 11:18:00 -0300

    refatoração

--- app/src/main/java/com/example/mytasklist/TaskApplication.kt
+++ app/src/main/java/com/example/mytasklist/TaskApplication.kt
@@ -8,7 +8,6 @@ import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
 
 class TaskApplication : Application() {
-
     override fun onCreate() {
         super.onCreate()
         startKoin {
